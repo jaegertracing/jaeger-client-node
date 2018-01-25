b595910702bed99befe7649390ff622513b699f4
Kara de la Marck <karadelamarck@gmail.com>
2018-01-25 19:51:11 +0000

    Revert report method in RemoteReporter
    Comment out new async report method in RemoteReporter
    Signed-off-by: Kara de la Marck <karadelamarck@gmail.com>

--- src/reporters/remote_reporter.js
+++ src/reporters/remote_reporter.js
@@ -45,22 +45,22 @@ export default class RemoteReporter {
         return 'RemoteReporter';
     }
 
-    // report(span: Span): void {
-    //     let response: SenderResponse = this._sender.append(ThriftUtils.spanToThrift(span));
-    //     if (response.err) {
-    //         this._logger.error('Failed to append spans in reporter.');
-    //         this._metrics.reporterDropped.increment(response.numSpans);
-    //     }
-    // }
-
     report(span: Span): void {
-        this._sender.append(ThriftUtils.spanToThrift(span), (error, response: SenderResponse) => {
-          if (error || response.err) {
-              this._logger.error('Failed to append spans in reporter.');
-              this._metrics.reporterDropped.increment(response.numSpans);
-          }
+        let response: SenderResponse = this._sender.append(ThriftUtils.spanToThrift(span));
+        if (response.err) {
+            this._logger.error('Failed to append spans in reporter.');
+            this._metrics.reporterDropped.increment(response.numSpans);
         }
     }
+
+    // report(span: Span): void {
+    //     this._sender.append(ThriftUtils.spanToThrift(span), (error, response: SenderResponse) => {
+    //       if (error || response.err) {
+    //           this._logger.error('Failed to append spans in reporter.');
+    //           this._metrics.reporterDropped.increment(response.numSpans);
+    //       }
+    //     }
+    // }
 
     flush(callback: ?Function): void {
         if (this._process === undefined) {
