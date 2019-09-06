4e52fa9ef8f74bda6327a71143896b1021d5b2bd
Yuri Shkuro <yurishkuro@users.noreply.github.com>
2019-09-06 16:31:55 -0400

    Export experimental samplers via index (#388)
    Signed-off-by: Yuri Shkuro <ys@uber.com>

--- test/samplers/exp_priority_sampler.js
+++ test/samplers/exp_priority_sampler.js
@@ -14,14 +14,13 @@
 import { assert } from 'chai';
 import sinon from 'sinon';
 import * as opentracing from 'opentracing';
-import Span from '../../src/span';
+import { ConstSampler, InMemoryReporter, Span, Tracer } from '../../src/index';
 import Utils from '../../src/util';
-import ConstSampler from '../../src/samplers/const_sampler';
-import TagEqualsSampler from '../../src/samplers/experimental/tag_equals_sampler';
-import PrioritySampler from '../../src/samplers/experimental/priority_sampler';
-import InMemoryReporter from '../../src/reporters/in_memory_reporter';
-import Tracer from '../../src/tracer';
 import BaseSamplerV2 from '../../src/samplers/v2/base';
+
+// import these from index to test 'experimental' export.
+var PrioritySampler = require('../../src/index').experimental.PrioritySampler;
+var TagEqualsSampler = require('../../src/index').experimental.TagEqualsSampler;
 
 describe('PrioritySampler with TagSampler', () => {
   const tagSampler = new TagEqualsSampler('theWho', [
