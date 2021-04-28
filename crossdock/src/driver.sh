6a6e3eab042e9c0a965b96b2eff2c9af87c6cc83
Yuri Shkuro <yurishkuro@users.noreply.github.com>
2021-04-27 21:53:19 -0400

    Stop testing with Node <v10; upgrade tchannel->4.x (#463)
    * Stop testing with Node <v10, add latest versions
    
    Signed-off-by: Yuri Shkuro <github@ysh.us>
    
    * update
    
    Signed-off-by: Yuri Shkuro <github@ysh.us>
    
    * Upgrade tchannel to 4.x, use node-alpine for crossdock
    
    Signed-off-by: Yuri Shkuro <github@ysh.us>
    
    * cleanup
    
    Signed-off-by: Yuri Shkuro <github@ysh.us>
    
    * cleanup
    
    Signed-off-by: Yuri Shkuro <github@ysh.us>
    
    * cleanup
    
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- crossdock/src/driver.sh
+++ crossdock/src/driver.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+set -x
+
 /node_modules/.bin/babel-node /crossdock/src/http_server.js &
 /node_modules/.bin/babel-node /crossdock/src/tchannel_server.js &
 
-sleep 10
+# unfortunately, we do not check that the above two servers are ready
+# before starting the healthcheck handler, so give them some time.
+sleep 20
+
 /node_modules/.bin/babel-node /crossdock/src/healthcheck_server.js &
 
 sleep infinity
